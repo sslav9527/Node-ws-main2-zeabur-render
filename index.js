9483ce58b84497a739307cf5ecff2798cd33e6c0
sslav9527 <97280575+sslav9527@users.noreply.github.com>
2024-04-21 23:12:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { exec } = require('child_process');
 const { WebSocket, createWebSocketStream } = require('ws');
 const logcb = (...args) => console.log.bind(this, ...args);
 const errcb = (...args) => console.error.bind(this, ...args);
-// const projectPageURL = `https://p01--magical-foot--mcrxjrdkw27n.code.run/`;
+// const projectPageURL = `https://p01--s9527--mcrxjrdkw27n.code.run`;
 const uuid = (process.env.UUID || 'de04add9-5c68-6bab-950c-08cd5320df37').replace(/-/g, "");
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nezha.sslav.eu.org:5555';
 const NEZHA_KEY = process.env.NEZHA_KEY || 'OHrAsflfUXCNrR5w9z';
